dcaa39e13c5fb86d9e537c48a55e9e67b17e3801
ezequiel-capaceta <108444379+ezequiel-capaceta@users.noreply.github.com>
2022-09-29 20:01:25 -0700

    commit

--- public/index.js
+++ public/index.js
@@ -3,6 +3,13 @@ function main(){
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
+    async function getWebPage(){
+        let response = await fetch('http://192.168.100.132:9001/counter')
+        let responseText = await response.json()
+        console.log(responseText)
+    }
+    getWebPage()
+
     let countValue = 0;
 
     function increment(){
